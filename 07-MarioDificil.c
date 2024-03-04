94b41da217a4d0aa8052255329dc68471a98d83a
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-03-03 22:36:43 -0300

    Create 07-MarioDificil.c

---
+++ 07-MarioDificil.c
@@ -0,0 +1,42 @@
+int main (void)
+{
+    int sharp;
+
+    do
+    {
+        printf("Digite o tamanho da escada de 1 a 8: ");
+        scanf("%i", &sharp);
+    }
+    while(sharp < 1 || sharp > 8);
+
+    for(int linha = 0; linha < sharp; linha++)
+    {
+        for(int coluna = 0; coluna < sharp; coluna++)
+        {
+            if (sharp - linha - 2 < coluna)
+            {
+                printf("#");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+
+        printf(" ");
+
+        for(int espelho = sharp; espelho > 0; espelho--)
+        {
+            if (sharp - espelho - 1 < linha)
+            {
+                printf("#");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+}
